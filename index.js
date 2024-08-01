6b125bb3609830c3f0fc4f928b347f4db90d13bf
LRlaxmirathod <168898815+LRlaxmirathod@users.noreply.github.com>
2024-08-01 16:26:50 +0530

    FUNCTION
    function

---
+++ index.js
@@ -0,0 +1,25 @@
+// find sum using function
+
+function sum(x , y){
+    console.log(x+y)
+}
+
+sum(1,2)
+
+//using arrow function calculate sum
+
+const suum = (a , b)=>{
+    console.log(a + b)
+}
+
+suum(3,4)
+
+// using return keyworn print sum
+
+function sum1(r , l){
+    return r + l
+}
+
+console.log(sum1(4,1))
+
+
